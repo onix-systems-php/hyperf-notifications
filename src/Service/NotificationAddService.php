173e95222af93a35ca094c42aab098dd83c5ba56
Alex Syvolap <oleksandr.syvolap@onix-systems.com>
2022-12-30 12:03:55 +0200

    feat: add validation

--- src/Service/NotificationAddService.php
+++ src/Service/NotificationAddService.php
@@ -11,30 +11,49 @@ declare(strict_types=1);
 namespace OnixSystemsPHP\HyperfNotifications\Service;
 
 use Hyperf\DbConnection\Annotation\Transactional;
+use Hyperf\Validation\Rule;
 use OnixSystemsPHP\HyperfCore\Constants\ErrorCode;
 use OnixSystemsPHP\HyperfCore\Exception\BusinessException;
 use OnixSystemsPHP\HyperfCore\Service\Service;
 use OnixSystemsPHP\HyperfNotifications\DTO\AddNotificationDTO;
 use OnixSystemsPHP\HyperfNotifications\Model\Notification;
 use OnixSystemsPHP\HyperfNotifications\Repository\NotificationRepository;
+use Hyperf\Validation\Contract\ValidatorFactoryInterface;
 
 #[Service]
 class NotificationAddService
 {
-    public function __construct(private NotificationRepository $rNotification)
-    {
+    public function __construct(
+        private NotificationRepository $rNotification,
+        private ValidatorFactoryInterface $vf,
+    ) {
     }
 
     #[Transactional(attempts: 1)]
     public function add(AddNotificationDTO $notificationData): Notification
     {
         return tap(
-            $this->rNotification->create($notificationData->toArray()),
+            $this->rNotification->create($this->validate($notificationData)),
             function (Notification $notification) {
                 if (! $this->rNotification->save($notification)) {
                     throw new BusinessException(ErrorCode::BAD_REQUEST_ERROR, __('exceptions.php.400'));
                 }
             }
         );
+    }
+
+    private function validate(AddNotificationDTO $notificationData): array
+    {
+        return $this->vf
+            ->make($notificationData->toArray(), [
+                'transport' => ['required', 'string', 'max:255'],
+                'type' => ['required', 'string', 'max:255'],
+                'target' => ['nullable', 'string', 'max:255'],
+                'target_id' => ['nullable', 'string', 'max:255'],
+                'title' => ['nullable', 'string', 'max:255'],
+                'image' => ['nullable', 'array'],
+                'image.id' => [Rule::exists('files', 'id')],
+            ])
+            ->validate();
     }
 }
