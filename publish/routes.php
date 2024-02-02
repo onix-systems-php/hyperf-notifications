adfb93ac7953e6a10c66048d082347651ab59cbd
Alex Syvolap <80973788+alexsyvolap@users.noreply.github.com>
2024-02-02 15:52:13 +0200

    fix(routes): fix index route

--- publish/routes.php
+++ publish/routes.php
@@ -10,7 +10,7 @@ use Hyperf\HttpServer\Router\Router;
 use OnixSystemsPHP\HyperfNotifications\Controller\NotificationsController;
 
 Router::addGroup('/v1/notifications', function () {
-    Router::get('/', [NotificationsController::class, 'index']);
+    Router::get('', [NotificationsController::class, 'index']);
     Router::get('/statistic', [NotificationsController::class, 'statistic']);
     Router::post('/{notificationId}/read', [NotificationsController::class, 'read']);
 });
